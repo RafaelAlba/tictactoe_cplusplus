af820dd1ad0bb7528e55ca5c193ef81cccf5235e
Rafael Alba <rafael.alba.lopez@gmail.com>
2013-11-05 16:28:33 -0800

    added the main menu, control structures, and error detection

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+
 using namespace std;
 
 int main()
 {
-  cout << "Hello World" << endl;
+  cout << "1. New Game" << endl;
+  cout << "2. Quit" << endl;
+  cout << endl;
+
+  cout << "Please select an option (1 or 2): ";
+
+  string choice;
+  getline(cin, choice);
+
+  int selectedOption;
+  stringstream(choice) >> selectedOption;
+
+  if (1 == selectedOption)
+    cout << "You chose to start a new game!" << endl;
+  else if (2 == selectedOption)
+    cout << "QUITTER!" << endl;
+  else
+    cout << "i don't know what you want" << endl;
+
   return 0;
 }
