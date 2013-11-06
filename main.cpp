0b0e711ca7107c1afeae78ebfd12aa5f8103b432
Rafael Alba <rafael.alba.lopez@gmail.com>
2013-11-05 17:25:25 -0800

    capturing player one and player two's name

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
   cout << "1. New Game" << endl;
-  cout << "2. Quit" << endl;
+  cout << "2. Quit" << endl << endl;
   cout << endl;
 
   cout << "Please select an option (1 or 2): ";
@@ -18,11 +18,24 @@ int main()
   int selectedOption;
   stringstream(choice) >> selectedOption;
 
-  if (1 == selectedOption)
-    cout << "You chose to start a new game!" << endl;
-  else if (2 == selectedOption)
+  if (1 == selectedOption) {
+    string playerOneName;
+    string playerTwoName;
+
+    cout << "Starting New Game!" << endl << endl;
+
+    cout << "Please enter a name for player one: ";
+    getline(cin, playerOneName);
+    cout << "Name for first player: " << playerOneName << endl;
+
+    cout << "Please enter a name for player two: ";
+    getline(cin, playerTwoName);
+    cout << "Name for second player: " << playerTwoName << endl;
+
+  } else if (2 == selectedOption) {
     cout << "QUITTER!" << endl;
-  else
+
+  } else
     cout << "i don't know what you want" << endl;
 
   return 0;
